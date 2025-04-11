d5ba23723b311b0a40f2b9abc71e2b93905afb76
ashutosh249022 <ashutosh_249022@saitm.ac.in>
2025-04-11 09:45:59 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,136 @@
+// // // console.log("--Welcome in js ----");
+
+// // // console.log("-----Staring with Variables-------a,b,c,");
+
+// // // const a = 5;
+// // // let b = 6;
+// // // var c =  7;
+
+// // // b = 8;
+
+
+
+// // // console.log("a")
+// // // console.log("-a : , b : , c : ", a, b, c );
+
+// // // console.log ("----sum --- :" + a + b + c);
+
+// // // const marks  = 80;
+
+
+// // // if (marks > 40){
+// // //     console.log("pass");
+
+// // // }else{
+// // //     console.log("fail");
+// // // }
+
+
+// // // console.log('largest no:');
+// // // const x = 5;
+// // // const y = 8;
+// // // const z = 67;
+
+
+// // // if  (x > z) {
+// // //     if( x > z){
+// // //         console.log(x);
+// // //     }
+
+    
+
+// // // }else{
+// // //     if (y > z ){
+// // //         console.log(y);
+// // //     }else{
+// // //         console.log(z);
+// // //     }
+    
+
+// // // }
+
+
+// // console.log("----For loops---- ");
+
+// // let sum = 0; // 0 1 2 3 4 5 6 7 8 9
+// // for (let  i = 0; i < 10; i++){
+// // sum + sum + i;
+// // } 
+
+// // console.log (sum);
+
+
+// // for (let i = 0; i < 10; i++){
+// //     console.log(i);
+
+// // }
+// // //console.log(sum);
+
+
+// console.log("------array-----");
+
+// // indexes : 01234567
+
+// // const  arr = [1,2,3,4,5,6,6,6,];
+
+// // console.log(arr);
+// // console.log("firrstvalue : ", arr[0]);
+
+// // for (let i = 0; i < arr, length; i++){
+// //     console.log(arr[i]);
+// // }
+
+// // const obj = {
+// //     name : "ashu",
+// //     rollno: 2345,
+// //     results:"pass",
+
+// // };
+// //  console.log(obj);
+// //  console.log(" name : ", obj ["name"]);
+
+// //  console.log ("----- add new key in obj----");
+// //  obj.id  = 2;
+
+// //   console.log(obj);
+
+// //    console.log("---remove elements from obj------");
+
+
+// //    delete obj.name;
+
+
+// //    console.log(obj);
+
+
+//    const data =[
+//     {
+//         id : 1,
+//         name : "a"
+//     },
+
+//    {
+//     id : 2,
+//     name : "b"
+// },
+// {
+//     id : 3,
+//     name : "c"
+// },
+// {
+//     id : 4,
+//     name : "d"
+// },
+// {
+//     id : 5,
+//     name : "e"
+// },
+// {
+//     id : 6,
+//     name : "f"
+// },
+//    ];
+
+
+ 
+
