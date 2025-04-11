eba35419428364d7d4f88b60cdb7a0a771136787
ashutosh249022 <ashutosh_249022@saitm.ac.in>
2025-04-11 12:59:51 +0530

    Add files via upload

---
+++ day 4/index.js
@@ -0,0 +1,25 @@
+// console.log("----welcome in js------");
+
+
+// const myHTML = `<div style="border:4px'solid'y
+//                 <h1>Added From JS</h1>
+//                 <h1>Added From JS</h1>
+//                 <h1> ashu</h1
+//                ` 
+//                ; 
+               
+
+
+// const  htmlEle = documents.getElementByID("container");
+// const  htmlEle2 = documents.getElementByIDByClassName("container2")[0];
+// htmlEle.innnerhtml = myHTML;
+// htmlEle2.innnerhtml = myHTML;
+
+
+
+
+for (let i = 0; i < data.lenght; i++){
+    const  myObj  = data [i];
+    const myHTML = `<h1>${myObj.name}</h1>`;
+    CSSContainerRule.innnerHTML;
+}
